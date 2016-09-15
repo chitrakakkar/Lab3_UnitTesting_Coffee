53f56993f7e00efeb917e2d2ff84e5ff5ff2b114
yp0975eu(Branden) <yp0975eu@users.noreply.github.com>
2016-09-15 16:52:58 -0500

    moved products to a class property

--- Drinks.py
+++ Drinks.py
@@ -1,18 +1,21 @@
-
 class drink():
+
+    products = {"coffee": [5.45, 4.15, 3.25],
+                "mocha": [4.65, 3.78, 2.65],
+                "tea": [4.23, 3.34, 2.54],
+                "cappuccino": [4.85, 3.28, 2.95],
+                "espresso": [4.35, 3.63, 2.45]}
+
     def __init__(self, name, size):
         self.name = name
         self.size = size
 
+    def get_products(self):
+        return self.products
+
     def calculate_price(self, quantity):
         """ The prices are listed like 'Large','Medium','Small'
          'Look -up is the index of price ar per the size """
-
-        products = {"coffee": [5.45, 4.15, 3.25],
-                    "mocha": [4.65, 3.78, 2.65],
-                    "tea": [4.23, 3.34, 2.54],
-                    "cappuccino": [4.85, 3.28, 2.95],
-                    "espresso": [4.35, 3.63, 2.45]}
 
         if str(self.size).strip(" ").upper() == 'LARGE':
             lookup = 0
@@ -20,9 +23,9 @@ class drink():
             lookup = 1
         else:
             lookup = 2
-        for key, data in products.items():
+        for key, data in self.products.items():
             if key == str(self.name).strip(" ").lower():
-                total_price = int(quantity) * products[key][lookup]
+                total_price = int(quantity) * self.products[key][lookup]
                 return total_price
 
 
