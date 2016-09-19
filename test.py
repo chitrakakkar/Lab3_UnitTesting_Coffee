021085a0f330dc7dfe0ebab9660b8eba5307ac05
chitrakakkar <India@123>
2016-09-19 15:18:10 -0500

    Valid_product_list done

--- test.py
+++ test.py
@@ -36,16 +36,28 @@ class PriceTest(unittest.TestCase):
         self.assertEqual(expected, actual, " Quantity should be positive integer")
 
     def test_valid_price_check(self):
+        # checks if the object has valid price
         drink1 = Drink("Mocha", " Small")
         expected = 2.65
         actual = drink1.products["mocha"][2]
         self.assertEqual(expected,actual, " The price for small mocha should be $2.65")
 
     def valid_drink_check(self):
+        # tests if the drink is valid
         drink1 = Drink("Tea", "Medium")
         expected = ["coffee", "mocha", "tea", "cappuccino", "espresso"]
         actual = (drink1.products.keys())
         self.assertListEqual(expected, actual)
+
+    def valid_product_list(self):
+        dict = {"coffee": [5.45, 4.15, 3.25],
+                "mocha": [4.65, 3.78, 2.65],
+                "tea": [4.23, 3.34, 2.54],
+                "cappuccino": [4.85, 3.28, 2.95],
+                "espresso": [4.35, 3.63, 2.45]}
+        actual = Drink.getproduct()
+        self.assertEqual(dict, actual, " Product list is fine")
+
 
 
 
