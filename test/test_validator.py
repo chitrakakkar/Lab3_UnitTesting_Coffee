81a74cfbddccdfd7c19df42ecddfb2310aec10d1
chitrakakkar <India@123>
2016-09-23 09:48:56 -0500

    Adding test modules

---
+++ test/test_validator.py
@@ -0,0 +1,14 @@
+""" Unit testing file for validation method """
+
+from unittest import TestCase
+from validator import *
+
+
+class TestValidator(TestCase):
+
+    def test_get_user_int(self):
+        # test if any string has been input
+        self.assertTrue(get_string_input("Hello"))
+        # test
+
+
